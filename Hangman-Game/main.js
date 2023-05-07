84cfefef3c56f5e58ea53af18fda8b2e8ad0b476
=ahmedyasser <=ahmedya3ser8@gmail.com>
2023-05-07 06:21:40 +0300

    End Game

--- Hangman-Game/main.js
+++ Hangman-Game/main.js
@@ -40,3 +40,50 @@ lettersAndSpace.forEach(letter => {
   }
   lettersGuessContainer.appendChild(emptySpan);
 });
+
+let guessSpans = document.querySelectorAll(".letters-guess span");
+
+let wrongTries = 0;
+
+let theDraw = document.querySelector(".hangman-draw");
+
+document.addEventListener("click", (e) => {
+  let theStatus = false;
+  if (e.target.className === "letter-box") {
+    e.target.classList.add("clicked");
+    let clickedLetter = e.target.innerHTML.toLowerCase();
+    let chosenWord = Array.from(randomValueName.toLowerCase());
+    chosenWord.forEach((wordLetter, indexWord) => {
+      if (clickedLetter === wordLetter) {
+        theStatus = true;
+        guessSpans.forEach((span, spanIndex) => {
+          if (indexWord === spanIndex) {
+            span.innerHTML = clickedLetter;
+          }
+        });
+      }
+      });
+    if (theStatus !== true) {
+      wrongTries++;
+      theDraw.classList.add(`wrong-${wrongTries}`);
+      document.getElementById("fail").play();
+      if (wrongTries === 8) {
+        endGame();
+        lettersContainers.classList.add("finished");
+        setTimeout(() => {
+          location.reload();
+        },1000)
+      }
+    } else {
+      document.getElementById("success").play();
+    }
+  }
+});
+
+function endGame() {
+  let div = document.createElement("div");
+  let divText = document.createTextNode(`Game Over, The Word is ${randomValueName}`);
+  div.appendChild(divText);
+  div.className = 'popup';
+  document.body.appendChild(div);
+}
